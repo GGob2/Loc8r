b6199f00f269199f7afb052429adbd10dd3d8cfa
GGOB2 <rkdaudwh13@naver.com>
2020-09-13 16:01:19 +0900

    asdf

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,8 +1,9 @@
-/* GET hompage */
 const index = (req, res) => {
-    res.render('index', {title : 'Express'})
+a  res.render("index", {
+    title: "Express by 2018250001 강명조",
+  });
 };
 
-module.exports ={
-    index
+module.exports = {
+  index,
 };
