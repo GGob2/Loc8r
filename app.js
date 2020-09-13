9ba874fa9c15eeedf87c58ee30fae2bdcdc4baae
GGOB2 <rkdaudwh13@naver.com>
2020-09-13 14:31:35 +0900

    modify structures in dir

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+const indexRouter = require('./app_server/routes/index');
+const userRouter = require('./app_server/routes/users');
 
 var app = express();
 
