1076091a03ea0c9ecf77d60b65a4751e1983cdd7
GGOB2 <rkdaudwh13@naver.com>
2020-09-20 03:19:53 +0900

    5-6 과제 완료

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,6 +1,6 @@
 /* GET hompage */
 const about = (req, res) => {
-    res.render('index', {title : 'About by 2018250001 강명조'})
+    res.render('generic-text', {title : 'About by 2018250001 강명조'})
 };
 
 module.exports ={
