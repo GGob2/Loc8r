10465998f424ec4fd10a81725d588d5f5887514b
GGOB2 <rkdaudwh13@naver.com>
2020-10-07 17:59:11 +0900

    10-1

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 
-
-// placeholder 함수 --> 아무기능도 하지 않음
 const locationsReadOne = (req, res) => {
-    res
-        .status(200)
-        .json({"status" : "success"});
+    Loc
+        .findById(req.params.locationid)
+        .exec((err, location)=> {
+            console.log("data 찾음" + location)
+            res
+                .status(200)
+                .json(location);
+        });
 };
 
+// placeholder 함수 --> 아무기능도 하지 않음
 const locationsListByDistance = (req, res) => {};
 const locationsCreate = (req, res) => {};
 const locationsUpdateOne = (req, res) => {};
