4e86a0a37a63dcbaad60f36d116f7174faaf2f3a
GGOB2 <rkdaudwh13@naver.com>
2020-11-08 00:02:08 +0900

    실습 14-1 완료

--- app.js
+++ app.js
@@ -26,7 +26,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api', apiRouter);
+// app.use('/api', apiRouter);
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, \
+  Content-Type, Accept, Authorization');
+  next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
