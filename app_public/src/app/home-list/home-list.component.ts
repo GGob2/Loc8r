0c05ec13cd43f9d47d2161b083e278f156522633
GGOB2 <rkdaudwh13@naver.com>
2020-11-08 01:02:20 +0900

    실습 14-2 완료

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export class Location {
+  _id: string;
+  name: string;
+  distance: number;
+  address: string;
+  rating: number;
+  facilities: string[];
+}
 @Component({
   selector: 'app-home-list',
   templateUrl: './home-list.component.html',
@@ -9,6 +18,24 @@ export class HomeListComponent implements OnInit {
 
   constructor() { }
 
+  name = 'Cafe de flore';
+
+  locations: Location[] = [{
+    _id: '5f7064b6419ed2739cb738f0',
+    name: 'Cafe de flore',
+    distance: 510.0,
+    address: '경기도 안성시 비룡5길 18',
+    rating: 4,
+    facilities: ['Hot drinks', 'Premium wifi', 'Food']
+  }, {
+    _id: '5f7064b6419ed2739cb738f2',
+    name: 'Cafe Jinny',
+    distance: 4510.0,
+    address: '경기도 안성시 고수1로 19',
+    rating: 2,
+    facilities: ['Hot drinks', 'Premium wifi', 'Food']
+  }];
+  
   ngOnInit(): void {
   }
 
