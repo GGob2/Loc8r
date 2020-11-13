4420a4527caf61adb71e5203bdf22dd4d26523dc
GGOB2 <rkdaudwh13@naver.com>
2020-11-14 02:55:03 +0900

    실습 17-2 완료

--- app_public/src/app/loc8r-data.service.ts
+++ app_public/src/app/loc8r-data.service.ts
@@ -13,9 +13,9 @@ export class Loc8rDataService {
   private apiBaseUrl = 'http://localhost:3000/api';
 
 
-  public getLocations(): Promise<Location[]> {
-    const lng: number = 127.264222;
-    const lat: number = 37.011639;
+  public getLocations(lat: number, lng: number): Promise<Location[]> {
+    // const lng: number = 127.264222;
+    // const lat: number = 37.011639;
     const maxDistance: number = 20000;
     const url: string = `${this.apiBaseUrl}/locations?lng=${lng}&lat=${lat}&maxDistance=${maxDistance}`;
     return this.http
